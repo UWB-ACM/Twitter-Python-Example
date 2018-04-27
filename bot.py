81b789019c7c8df25cb08d83d8238a33070134c6
Chris Johnston <chris@thejohnstons.net>
2018-04-27 14:04:07 -0700

    add cleanup to the bot.py file

--- bot.py
+++ bot.py
@@ -1,34 +1,63 @@
+"""
+Twitter Python Example
+
+Barebones example of using the Tweepy library for Python
+Logs into the service as a specific user, and gets their username.
+
+More examples can be found on the official documentation:
+http://docs.tweepy.org/en/v3.6.0/getting_started.html
+
+Usage:
+
+python3 bot.py /path/to/config.ini
+"""
+
+# use the configparser util for ez config files
 import configparser, sys
+# tweepy library, install this using the requirements.txt file
 import tweepy
 
-# load the configuration from the config file
-# another common way to do this is using environment variables
-config = configparser.ConfigParser()
+if __name__ == '__main__':
 
-print('Using config file located at: ', sys.argv[1])
+    # check the number of args
+    config_file_path = ''
 
-# read the file specified as first arg
-with open(sys.argv[1], 'r') as f:
-    config.read_file(f)
+    if len(sys.argv) > 1:
+        config_file_path = sys.argv[1]
+    else:
+        print('Usage: %s config.ini' % (sys.argv[0],))
+        sys.exit()
 
-# load the key and secrets from the config file
-# be careful with these, they grant anyone access to your account
-consumer_key = config['Twitter']['consumer_key']
-consumer_secret = config['Twitter']['consumer_secret']
+    # load the configuration from the config file
+    # another common way to do this is using environment variables
+    config = configparser.ConfigParser()
 
-access_token = config['Twitter']['access_token']
-access_token_secret = config['Twitter']['access_token_secret']
+    # print for debugging
+    print('Using config file located at: ', sys.argv[1])
 
-# logs in using the user authorized
-# using demo from http://tweepy.readthedocs.io/en/v3.6.0/getting_started.html
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+    # read the config file and use it for the config parser
+    with open(config_file_path, 'r') as f:
+        config.read_file(f)
 
-# get the api w/ the auth specified
-api = tweepy.API(auth)
+    # load the key and secrets from the config file
+    # be careful with these, they grant anyone access to your account
+    consumer_key = config['Twitter']['consumer_key']
+    consumer_secret = config['Twitter']['consumer_secret']
 
-# print some debug stuff
-print('tweepy version ' + tweepy.__version__)
-logged_in_user = api.me()
-print('connected to api as user', logged_in_user.name)
+    access_token = config['Twitter']['access_token']
+    access_token_secret = config['Twitter']['access_token_secret']
 
+    # logs in using the user authorized
+    # using demo from http://tweepy.readthedocs.io/en/v3.6.0/getting_started.html
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    # get the api w/ the auth specified
+    api = tweepy.API(auth)
+
+    # print some debug stuff
+    print('tweepy version ' + tweepy.__version__)
+    # logged into the service, get the username
+    # after now, should be able to mess with the api
+    logged_in_user = api.me()
+    print('connected to api as user', logged_in_user.name)
